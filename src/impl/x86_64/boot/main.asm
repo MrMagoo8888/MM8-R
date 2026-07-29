9237a8e71ec114ba0b7ae21dceb907af1face781
MrMagoo8888 <mrmagoo8888@yahoo.com>
2026-07-29 15:22:03 +0100

    IT GETS TO 64 BIT KERNEL MAIN64

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,5 +1,6 @@
 global header_start
-global _start                   ; Usually required by linkers
+global start                   ; Usually required by linkers
+extern long_mode_start
 
 section .text
 [bits 32]
@@ -12,10 +13,13 @@ start:
     call check_cpuid
     call check_long_mode
 
+    call setup_page_tables
+    call enable_paging
+
+    lgdt [gdt64.pointer]
+    jmp gdt64.code_segment:long_mode_start
     
 
-    cli
-    hlt
 
 ; ============================================================================
 ; Validation Routines
@@ -41,7 +45,7 @@ check_cpuid:
     push ecx
     popfd
     cmp eax, ecx
-    je .no_cpu
+    je .no_cpuid
     ret
 .no_cpuid:
     mov bl, "C"
@@ -51,7 +55,7 @@ check_long_mode:
     mov eax, 0x80000000
     cpuid
     cmp eax, 0x80000001
-    jmp .no_long_mode
+    jz .no_long_mode
 
     mov eax, 0x80000001
     cpuid
@@ -59,9 +63,55 @@ check_long_mode:
     jz .no_long_mode
 
     ret
-.no_long_mode:
+.no_long_mode:      ; I get error here
     mov bl, "L"
     jmp trigger_error
+
+setup_page_tables:
+    mov eax, page_table_l3
+    or eax, 0b11    ; Present, writable
+    mov [page_table_l4], eax
+
+    mov eax, page_table_l2
+    or eax, 0b11    ; Present, writable
+    mov [page_table_l3], eax
+
+    mov ecx, 0  ; counter
+.loop:
+
+    mov eax, 0x200000   ; 2MiB
+    mul ecx
+    or eax, 0b10000011  ; Present, writable, huge page ;)
+    mov [page_table_l2 + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512    ; Check if whole table is mapped
+    jne .loop       ; if not, continue
+
+    ret
+
+enable_paging:
+    ; pass table location into cpu
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    ; Enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; enable long-john silver mode
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; Enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
    
 
 ; ==================================================================
@@ -111,7 +161,27 @@ section .data
     error_template: db "ERR: X", 0
 
 section .bss
-align 16                        ; System V ABI requires 16-byte stack alignment me thinks
+align 4096                        ; System V ABI requires 16-byte stack alignment me thinks
+page_table_l4:
+    resb 4096
+page_table_l3:
+    resb 4096
+page_table_l2:
+    resb 4096
+
 stack_bum:
     resb 4096 * 4
 stack_top:
+
+
+
+section .rodata
+
+gdt64:
+    dq 0 ; entry
+.code_segment: equ $ - gdt64
+    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
+
