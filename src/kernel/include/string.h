409aea90d04d9e67cb560cbd5a55664a12964cb4
MrMagoo8888 <mrmagoo8888@yahoo.com>
2026-08-09 20:42:14 +0100

    still boots, more string funcs

--- src/kernel/include/string.h
+++ src/kernel/include/string.h
@@ -4,3 +4,14 @@ void* memcpy (void *dest, const void *src, size_t len);
 int memcmp (const void *str1, const void *str2, size_t count);
 void* memmove (void *dest, const void *src, size_t len);
 void* memset (void *dest, int val, size_t len);
+
+char* strncpy(char *s1, const char *s2, size_t n);
+char* strcpy(char *dest, const char *src);
+
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+
+char* strrchr(const char *s, int c);
+char* strchr(const char *s, int c);
+
+size_t strlen(const char *s);
--- src/kernel/libs/string.c
+++ src/kernel/libs/string.c
@@ -2,7 +2,7 @@
 #include "stdint.h" 
 
 void* memset(void *dest, int val, size_t len) {
-    
+
     unsigned char *ptr = dest;
     unsigned char c = (unsigned char)val;
 
@@ -109,7 +109,7 @@ static size_t internal_strnlen(const char *s, size_t maxlen) {
     return len;
 }
 
-char* strncpy(char *s1, const char *s2, size_t n) {
+char* strncpy(char *s1, const char *s2, size_t n) {     // might page fault
 
     size_t size = internal_strnlen(s2, n);
     memcpy(s1, s2, size);
@@ -118,3 +118,98 @@ char* strncpy(char *s1, const char *s2, size_t n) {
     }
     return s1;
 }
+
+char* strcpy(char *dest, const char *src) {  // might page fault
+    size_t len = 0;
+    
+    // Find the length of the string
+    while (src[len] != '\0') {
+        len++;
+    }
+    
+    // cpy the string data + 1 byte for the '\0' terminator
+    memcpy(dest, src, len + 1);
+    
+    return dest;
+}
+
+int strcmp(const char *s1, const char *s2) {         // might page fault
+    const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+
+    while (*p1 && (*p1 == *p2)) {
+        p1++;
+        p2++;
+    }
+
+    // return negative value if s1 < s2, 0 if equal, positive if s1 > s2
+    return *p1 - *p2;
+}
+
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+    // if n is 0, the strings are equal by def
+    if (n == 0) return 0;
+
+    const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+
+    // decrement n each iterationstop if characters mismatch or \0 
+    while (n > 1 && *p1 && (*p1 == *p2)) {
+        p1++;
+        p2++;
+        n--;
+    }
+
+    // eval final character difference
+    return *p1 - *p2;
+}
+
+
+char* strchr(const char *s, int c) {
+    char target = (char)c;
+
+    // scan until found character or hit null term
+    while (*s != target) {
+        if (*s == '\0') {
+            return NULL; // char not found
+        }
+        s++;
+    }
+
+    // return the pointer casting away constness
+    return (char *)s;
+}
+
+
+
+char* strrchr(const char *s, int c) {
+    char target = (char)c;
+    size_t len = 0;
+
+    // find end of string
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    size_t i = len + 1; // include null terminator in search space
+    while (i > 0) {
+        i--;
+        if (s[i] == target) {
+            return (char *)(s + i); // found rightmost match
+        }
+    }
+
+    return NULL; // cahr not found
+}
+
+
+
+size_t strlen(const char *s) {
+    const char *p = s;
+    while (*p != '\0') {
+        p++;
+    }
+    return (size_t)(p - s);
+}
+
